2f0bd7c3969d008af84de85b7329f440c27144a4
iswarm <yuxuecai@gmail.com>
2018-08-02 11:02:05 +0800

    got in stuck when go get cli

--- kimGo.go
+++ kimGo.go
@@ -1,37 +1,19 @@
 package main
 
 import "fmt"
-import (
-    "crypto/aes"
-    "crypto/cipher"
-    "crypto/rand"
-    "encoding/hex"
-    "io"
-)
+import "os"
+import "github.com/urfave/cli.v1"
 
 
 func main(){
-    key,_ := hex.DecodeString("6368616e676520746869732070617373")
-    fmt.Printf("%d\n", aes.BlockSize)
-    str := "exampleplaintext"
-    fmt.Printf("%d\n", len(str))
-    plaintext := []byte("exampleplaintextexampleplaintextexampleplaintext")
-    if len(plaintext)%aes.BlockSize != 0 {
-        panic("plaintext is not a multiple of the block size")
+    app := cli.NewApp()
+    app.Name = "greet"
+    app.Usage = "say a greeting"
+    app.Action = func(c *cli.Context) error {
+        println("Greetings")
+        return nil
     }
-
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        panic(err)
-    }
-    ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-    iv := ciphertext[:aes.BlockSize]
-    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-        panic(err)
-    }
-    mode := cipher.NewCBCEncrypter(block, iv)
-    mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
-    fmt.Printf("%x\n",ciphertext)
+    app.Run(os.Args)
 }
 
 
