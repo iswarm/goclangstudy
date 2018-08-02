107d0a72bfa74f479fec4e27d40ae358036ab228
iswarm <yuxuecai@gmail.com>
2018-08-02 11:34:19 +0800

    bytes.Buffer and log, os.Open , os. Read

--- kimGo.go
+++ kimGo.go
@@ -2,18 +2,34 @@ package main
 
 import "fmt"
 import "os"
-import "github.com/urfave/cli.v1"
+import "log"
+import "bytes"
+//import "github.com/ethereum/go-ethereum/log"
+//import "github.com/urfave/cli.v1"
 
 
 func main(){
-    app := cli.NewApp()
-    app.Name = "greet"
-    app.Usage = "say a greeting"
-    app.Action = func(c *cli.Context) error {
-        println("Greetings")
-        return nil
+    file, err := os.Open("/Users/mac/goclangstudy/README.md")
+    if err != nil {
+        log.Fatal(err)
     }
-    app.Run(os.Args)
+    
+        data:=make([]byte, 1000)
+        count,err:=file.Read(data)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("read %d bytes: %q\n", count, data[:count])
+
+    var (
+        buf bytes.Buffer
+        logger = log.New(&buf, "logger: ", log.Lshortfile)
+    )
+
+    logger.Print("Hello, log file!")
+
+    fmt.Print(&buf)
+   
 }
 
 
