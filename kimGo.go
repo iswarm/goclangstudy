ee06bf4a379f4551b7c8a321fc78b1920f0cbf91
iswarm <yuxuecai@gmail.com>
2018-08-02 21:10:25 +0800

    cli package app

--- kimGo.go
+++ kimGo.go
@@ -4,8 +4,9 @@ import "fmt"
 import "os"
 import "log"
 import "bytes"
+import "path/filepath"
 //import "github.com/ethereum/go-ethereum/log"
-//import "github.com/urfave/cli.v1"
+import "github.com/urfave/cli"
 
 
 func main(){
@@ -26,10 +27,32 @@ func main(){
         logger = log.New(&buf, "logger: ", log.Lshortfile)
     )
 
-    logger.Print("Hello, log file!")
+    logger.Print("Hello, log file!\n")
 
     fmt.Print(&buf)
-   
+    fmt.Print("filepath.Base(os.Args[0]) = \n", filepath.Base(os.Args[0]))
+    app := cli.NewApp()
+    app.Flags = []cli.Flag{
+        cli.StringFlag{
+            Name: "lang",
+            Value:"english",
+            Usage: "language for the greeting",
+        },
+    }
+
+    app.Action = func(c *cli.Context) error {
+        name :="Nefertiti"
+        if c.NArg() >0 {
+            name = c.Args().Get(0)            
+        }
+        if c.String("lang") == "spanish" {
+            fmt.Println("Hola", name)
+        } else {
+            fmt.Println("Hello", name)
+        }
+        return nil
+    }
+    app.Run(os.Args)
 }
 
 
